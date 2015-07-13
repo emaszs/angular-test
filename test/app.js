35459cdb54d13105571b5739afc916ab5f450790
Emilis <phonixreal@gmail.com>
2015-07-13 16:40:23 +0200

    added a dynamic page loading prototype by using ng-view, routeProvider and  angular thingies.

--- test/app.js
+++ test/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('CRABMonitor', []);
+var app = angular.module('CRABMonitor', ['ngRoute']);
 
 //app.controller('DataController', ['$scope', '$http', function ($scope, $http) {
 //
@@ -11,32 +11,44 @@ var app = angular.module('CRABMonitor', []);
 //    })
 //}]);
 
-app.controller('TaskSearchController', ['$scope', '$http', function ($scope, $http) {
-    $scope.data = {};
-    $scope.searchTaskInfo = function () {
-        console.log("success");
-        // Retrieves task information from the api.
-        // In case of error (such as when something wasn't found), null the data and set receivedError to true
-        if ($scope.searchPhrase) {
-            $http.get('https://mmascher-mon.cern.ch/crabserver/dev/task?subresource=search&workflow=' +
-                $scope.searchPhrase).success(function (data) {
-                $scope.data = data;
-                console.log(data.result[0]);
-                $scope.receivedError = false;
-                $scope.displayTable = true;
-            }).
-            error(function () {
-                $scope.data = {};
-                $scope.receivedError = true;
-                $scope.displayTable = false;
-            });
-            //        $http({
-            //            url:'https://mmascher-mon.cern.ch/crabserver/dev/task?subresource=search&workflow='
-            //            method: "GET",
-            //            params:
-            //        })
-        }
-    }
+app.controller('TaskSearchController', ['$scope', '$http', '$location', function ($scope, $http) {
+	$scope.data = {};
+	$scope.searchTaskInfo = function () {
+		console.log("success");
+		// Retrieves task information from the api.
+		// In case of error (such as when something wasn't found), null the data and set receivedError to true
+		if ($scope.searchPhrase) {
+			$http.get('https://mmascher-mon.cern.ch/crabserver/dev/task?subresource=search&workflow=' +
+				$scope.searchPhrase).success(function (data) {
+				$scope.data = data;
+				console.log(data.result[0]);
+				$scope.receivedError = false;
+				$scope.displayTable = true;
+			}).
+			error(function () {
+				$scope.data = {};
+				$scope.receivedError = true;
+				$scope.displayTable = false;
+			});
+		}
+	}
 
 
+}]);
+
+app.controller('OtherViewController', ['$location', '$scope',
+								  function ($location, $scope) {
+
+		$scope.showSomethingElse = function () {
+			$location.path('/other_view');
+			console.log("testing");
+		}
+								  }]);
+
+app.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider.
+	when('/other_view', {
+		templateUrl: '/crabserver/ui/static?test/other_view.html',
+		controller: 'OtherViewController'
+	})
 }]);
