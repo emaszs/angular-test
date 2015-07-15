f400bc830ed00c83b2c6b4850d1063943d9139ad
Emilis <phonixreal@gmail.com>
2015-07-15 13:47:28 +0200

    some styling, basic error handling for task info search. trying to make the untarring lib work.

--- test/search.js
+++ test/search.js
@@ -1,35 +1,74 @@
-function displayTaskInfo() {
-	var searchPhrase = $("#task-search-form-input").val();
-	var xmlhttp = new XMLHttpRequest();
-	var url = "https://mmascher-mon.cern.ch/crabserver/dev/task?subresource=search&workflow=";
+$(document).ready(function () {
 
-	xmlhttp.onreadystatechange = function () {
-		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-			var data = JSON.parse(xmlhttp.response);
+	/**
+	 * Called on task info search form submission. It then fetches JSON data
+	 * and inserts it into the task_info.html #task-info-table.
+	 */
+	function displayTaskInfo() {
+		var searchPhrase = $("#task-search-form-input").val();
+		var xmlhttp = new XMLHttpRequest();
+		var url = "https://mmascher-mon.cern.ch/crabserver/dev/task?subresource=search&workflow=";
 
+		// Response processing
+		xmlhttp.onreadystatechange = function () {
 			// Emptying table in case of previous request
 			$("#task-info-table tbody").empty();
-			// Creating table contents
-			insertDataToView(data);
 
-			// Show table now that info has been loaded and inserter
-			$("#task-info-table").css("display", "inline");
+			if (xmlhttp.readyState == 4) {
+				if (xmlhttp.status == 200) {
+					console.log("OK");
+					var data = JSON.parse(xmlhttp.response);
+
+					// Creating table contents
+					insertDataToView(data);
+
+					$("#task-search-error-message").css("display", "none");
+					// Show table now that info has been loaded and inserted
+					$("#task-info-table").css("display", "inline");
+				} else {
+					// Displaying error message from response
+					console.log("Error");
+					$("#task-search-error-message").css("display", "inline")
+						.text(xmlhttp.status + " " + xmlhttp.statusText);
+				}
+			}
+		};
+
+		// Helper function.
+		// Inserts task info column names and values into #task-info-table.
+		function insertDataToView(data) {
+			for (i = 0; i < data.desc.columns.length; i++) {
+				$("#task-info-table tbody")
+					.append("<tr><td>" + data.desc.columns[i] + "</td><td>" + data.result[i] + "</td></tr>");
+			}
 		}
+
+		// Sends get request for JSON data
+		xmlhttp.open("GET", url + searchPhrase, true);
+		xmlhttp.send();
 	}
 
-	xmlhttp.open("GET", url + searchPhrase, true);
-	xmlhttp.send();
+	/**
+	 * Task info search form listener
+	 */
+	$("#task-search-form").submit(function () {
+		displayTaskInfo();
+		return false;
+	});
 
-	function insertDataToView(data) {
-		for (i = 0; i < data.desc.columns.length; i++) {
-			// Appending column names and data to task-info-table
-			$("#task-info-table tbody")
-				.append("<tr><td>" + data.desc.columns[i] + "</td><td>" + data.result[i] + "</td></tr>");
-		}
-	}
+
+
+
+})
+
+function untarTest() {
+	GZip.load("//vocms059.cern.ch/mon/cms1425/" +
+		+"150714_090340:erupeika_crab_tutorial_May2015_MC_analysis/sandbox.tar.gz",
+		function (h) {
+			var tar = new TarGZ;
+			tar.parseTar(h.data.join(''));
+			tar.files.forEach(console.log("success"));
+		}, null, null);
+
+
 }
-
-$("#task-search-form").submit(function () {
-	displayTaskInfo();
-	return false;
-});
